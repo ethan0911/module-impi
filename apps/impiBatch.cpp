a930da90fe0514768b85a40f4c29d88cf8c72d07
Qi <qwu@sci.utah.edu>
2018-03-08 23:59:51 -0700

    adjust parameter epsilon to 0.001 for matching viewer image

--- apps/impiBatch.cpp
+++ apps/impiBatch.cpp
@@ -9,11 +9,12 @@
 
 using namespace ospcommon;
 
+static bool showVolume = false;
 static float isoValue = 0.0f;
 static vec2i imgSize{1024, 768};
 static vec3f vp{43.2,44.9,-57.6};
-static vec3f vd{-0.534,-0.456,0.712};;
 static vec3f vu{0,1,0};
+static vec3f vi{0,0,0};
 static std::vector<vec3f> colors = {
   vec3f(0.0, 0.000, 0.563),
   vec3f(0.0, 0.000, 1.000),
@@ -73,12 +74,18 @@ int main(int ac, const char** av)
       else if (str == "-vp") { 
 	ospray::impi::Parse<3>(ac, av, i, vp);
       }
-      else if (str == "-vd") { 
-	ospray::impi::Parse<3>(ac, av, i, vd);
+      else if (str == "-vi") {
+	ospray::impi::Parse<3>(ac, av, i, vi);
       }
       else if (str == "-vu") { 
 	ospray::impi::Parse<3>(ac, av, i, vu);
       }    
+      else if (str == "-volume") { 
+	showVolume = true;
+      }
+      else if (str[i] == '-') {
+	throw std::runtime_error("unknown argument: " + str);
+      }
     }
   }
   if (args.inputFiles.empty()) { throw std::runtime_error("missing input file"); }
@@ -102,7 +109,9 @@ int main(int ac, const char** av)
   // setup volume
   auto amrVolume = ospray::ParseOSP::loadOSP(args.inputFiles[0]);
   OSPVolume volume = amrVolume->Create(transferFcn);
-  ospAddVolume(world, volume);
+  if (showVolume) {
+    ospAddVolume(world, volume);
+  }
 
   // setup isosurface
   OSPGeometry isosurface = ospNewGeometry("impi"); 
@@ -119,6 +128,7 @@ int main(int ac, const char** av)
 
   // setup camera
   OSPCamera camera = ospNewCamera("perspective");
+  vec3f vd = vi - vp;
   ospSetVec3f(camera, "pos", (const osp::vec3f&)vp);
   ospSetVec3f(camera, "dir", (const osp::vec3f&)vd);
   ospSetVec3f(camera, "up",  (const osp::vec3f&)vu);
@@ -127,53 +137,60 @@ int main(int ac, const char** av)
   ospCommit(camera);
 
   // setup lighting
-  OSPLight a_light = ospNewLight(renderer, "AmbientLight");
-  ospSet1f(a_light, "intensity", 0.90f);
-  ospSetVec3f(a_light, "color", osp::vec3f{174.f/255.f,218.f/255.f,255.f/255.f});
-  ospCommit(a_light);
   OSPLight d_light = ospNewLight(renderer, "DirectionalLight");
   ospSet1f(d_light, "intensity", 0.25f);
+  ospSet1f(d_light, "angularDiameter", 0.53f);
   ospSetVec3f(d_light, "color", osp::vec3f{127.f/255.f,178.f/255.f,255.f/255.f});
   ospSetVec3f(d_light, "direction", osp::vec3f{.372f,.416f,-0.605f});
   ospCommit(d_light);
   OSPLight s_light = ospNewLight(renderer, "DirectionalLight");
   ospSet1f(s_light, "intensity", 1.50f);
-  ospSetVec3f(s_light, "color", osp::vec3f{1.f,255.f/255.f,255.f/255.f});
+  ospSet1f(s_light, "angularDiameter", 0.53f);
+  ospSetVec3f(s_light, "color", osp::vec3f{1.f,1.f,1.f});  
   ospSetVec3f(s_light, "direction", osp::vec3f{-1.f,0.679f,-0.754f});
   ospCommit(s_light);
+  OSPLight a_light = ospNewLight(renderer, "AmbientLight");
+  ospSet1f(a_light, "intensity", 0.90f);
+  ospSetVec3f(a_light, "color", osp::vec3f{174.f/255.f,218.f/255.f,255.f/255.f});
+  ospCommit(a_light);
   std::vector<OSPLight> light_list { a_light, d_light, s_light };
   OSPData lights = ospNewData(light_list.size(), OSP_OBJECT, light_list.data());
   ospCommit(lights);
 
   // setup world & renderer
   ospCommit(world); 
-  ospSetVec3f(renderer, "bgColor", osp::vec3f{0.5f, 0.5f, 0.5f});
+  ospSetVec3f(renderer, "bgColor", osp::vec3f{230.f/255.f, 230.f/255.f, 230.f/255.f});
   ospSetData(renderer, "lights", lights);
   ospSetObject(renderer, "model", world);
   ospSetObject(renderer, "camera", camera);
-  ospSet1i(renderer, "shadowEnabled", 0);
-  ospSet1i(renderer, "oneSidedLighting", 0);
+  ospSet1i(renderer, "shadowEnabled", 1);
+  ospSet1i(renderer, "oneSidedLighting", 1);
+  ospSet1i(renderer, "maxDepth", 5);
+  ospSet1i(renderer, "spp", 1);
+  ospSet1i(renderer, "aoSamples", 1);
+  ospSet1i(renderer, "autoEpsilon", 1);
+  ospSet1i(renderer, "aoTransparencyEnabled", 1);
+  ospSet1f(renderer, "aoDistance", 10000.f);
+  ospSet1f(renderer, "epsilon", 0.001f);
   ospCommit(renderer);
 
   // setup framebuffer
   OSPFrameBuffer fb = ospNewFrameBuffer((const osp::vec2i&)imgSize, 
-					OSP_FB_SRGBA,
-					OSP_FB_COLOR | 
-					OSP_FB_ACCUM);
+					OSP_FB_SRGBA, OSP_FB_COLOR | OSP_FB_ACCUM);
   ospFrameBufferClear(fb, OSP_FB_COLOR | OSP_FB_ACCUM);
 
   // skip some frames to warmup
-  for (int frames = 0; frames < 10; frames++) {
+  for (int frames = 0; frames < 1; frames++) {
     ospRenderFrame(fb, renderer, OSP_FB_COLOR | OSP_FB_ACCUM);
   }
 
   // render 10 more frames, which are accumulated to result in a better converged image
   auto t = Time();
-  for (int frames = 0; frames < 200; frames++) {
+  for (int frames = 0; frames < 10; frames++) {
     ospRenderFrame(fb, renderer, OSP_FB_COLOR | OSP_FB_ACCUM);
   }
   auto et = Time(t);
-  std::cout << "#osp:bench: framerate " << 200/et << std::endl; 
+  std::cout << "#osp:bench: framerate " << 10/et << std::endl; 
 
   // save frame
   const uint32_t * buffer = (uint32_t*)ospMapFrameBuffer(fb, OSP_FB_COLOR);
