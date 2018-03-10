aa9f389ca9c30243e5562f82acbc86b8a018388f
Qi <qwu@sci.utah.edu>
2018-03-10 14:06:23 -0700

    add example (commented out) in chombo_bench.sh

--- apps/scripts/chombo_bench.sh
+++ apps/scripts/chombo_bench.sh
@@ -9,3 +9,14 @@ IMPI_AMR_METHOD=octant \
 -vi 8.000000 7.999991 7.999997 \
 -frames 50 200 \
 "$@"
+
+# IMPI_AMR_METHOD=octant \
+# ./ospImplicitIsoSurfaceBench \
+# /home/sci/feng/Desktop/ws/data/chombo/chombo_amr.osp \
+# -isos 2 0.3 0.7 \
+# -use-builtin-isosurface \
+# -vp 24.684797 17.313093 -10.046009 \
+# -vu 0.000000 1.000000 0.000000 \
+# -vi 8.000000 7.999991 7.999997 \
+# -frames 50 200 \
+# "$@"
