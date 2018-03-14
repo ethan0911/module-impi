f8e7ce5dd6a133ba57bd8062a035a23e5996cc59
Feng <fwang911@gmail.com>
2018-03-13 20:59:02 -0600

    modify code

---
+++ apps/scripts/landing_widget.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+MPI_AMR_METHOD=octant ./ospImplicitIsoSurfaceWidget /home/sci/feng/Desktop/ws/data/NASA_LandingGear_AMR/cb.osp -iso 99000.0 -object ~feng/Desktop/ws/data/NASA_LandingGear_AMR/landingGear.obj -vp 16.103643 15.418482 0.265052 -vu -0.000000 -0.000000 -1.000000 -vi 16.387318 15.844191 0.332234 -sun 0.337 0.416 -0.605 -dis 0.554 1.000 -0.211 -scale 1 1 1 -translate 15.995 16 0.1 -volume 
