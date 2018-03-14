df72bb3f6641cde2a98ef0b817d375a8174d9166
Qi <qwu@sci.utah.edu>
2018-03-13 21:32:47 -0600

    add script

---
+++ apps/scripts/cosmos_bench_tacc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PREFIX=$1
+
+$PREFIX/ospImplicitIsoSurfaceBench \
+/home/sci/feng/Desktop/ws/data/COSMOS_ospray/cosmos.osp \
+-iso 0.0 \
+-vp 814.398254 -54.506733 878.914001 \
+-vu 1.000000 0.000000 0.000000 \
+-vi 248.845215 267.241455 256.334686 \
+-frames 10 100 \
+"${@:2}"
